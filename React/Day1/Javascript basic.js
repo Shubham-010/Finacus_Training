3345479df38df4e0ae7306906124bb45bf887542
ShubhamC <Shubhamchikane0109@gmail.com>
2021-09-26 23:31:45 +0530

    react Day-1 updates

---
+++ React/Day1/Javascript basic.js
@@ -0,0 +1,78 @@
+/*const myfun =(name)=>{
+  console.log(name);
+}
+
+myfun('Shubham');*/
+/*class Human{
+  constructor(){
+    this.gender='Male';
+  }
+
+  printGender(){
+    console.log(this.gender);
+  }
+}
+class Person extends Human {
+  constructor(){
+    super();
+    this.name = 'Max';
+  }
+
+   printMyName(){
+     console.log(this.name);
+   }
+  }
+const person = new Person();
+person.printMyName(); person.printGender();*/
+/*spread elements*/
+/*const numbers =[1,2,3];
+const newnumber =[...numbers,4];
+
+console.log(newnumber);*/
+
+/*const number = {
+  name='Shub'
+};
+const age = {
+  ...number,
+    age=6
+}
+
+console.log(age);*/
+/*rest*/
+/*const filter = (...args) => {
+  return args.filter(el => el === 2);
+}
+
+console.log(filter(1,2,3));
+*/
+/* Array Destructuring*/
+/*const numbers = [1,2,3];
+[num1, num2 ,num3] = numbers;
+console.log(num1,num2,num3);*/
+
+/*reference and primitive*/
+/*const person = {
+  name:'sam'
+};
+const sperson = person;
+console.log(sperson);
+*/
+const numbers =[1,2,3];
+const doubleNumArray = numbers.map((map) =>{});
+
+console.log(numbers);
+console.log(doubleNumArray);
+
+
+
+
+
+
+
+
+
+
+
+
+person.printGender
